6067123e54bac7bf414a86b146c39a24fe63da31
secondlnl <142692021+secondlnl@users.noreply.github.com>
2025-09-01 21:52:35 +0200

    tut 3 script beginning just added to player

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    { 
+        if(Input.GetKey(KeyCode.R)) {}
+    }
+
+}

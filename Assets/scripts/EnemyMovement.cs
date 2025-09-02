8b4cafe4b8e2360ed485eab4c182a5289fd28bdc
secondlnl <142692021+secondlnl@users.noreply.github.com>
2025-09-02 21:51:22 +0200

    Enemy

---
+++ Assets/scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float MoveSpeed = 2.0f;
+    [SerializeField] private float bounciness = 300f;
+    private SpriteRenderer sr;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector2(MoveSpeed, 0) * Time.deltaTime);
+
+        if (MoveSpeed > 0) sr.flipX = true;
+        if (MoveSpeed < 0) sr.flipX = false;
+
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("EnemyBlock")) MoveSpeed = -MoveSpeed;
+        if (other.gameObject.CompareTag("Enemy")) MoveSpeed = -MoveSpeed;
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(other.GetComponent<Rigidbody2D>().linearVelocityX, 0);
+            other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, bounciness));
+        }
+        Destroy(gameObject);
+    }
+}

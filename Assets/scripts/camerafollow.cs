1eaa198f69d22a9162bd39799c435ea8b69fe8a4
secondlnl <142692021+secondlnl@users.noreply.github.com>
2025-09-02 21:10:02 +0200

    @Camera now follows you

---
+++ Assets/scripts/camerafollow.cs
@@ -0,0 +1,15 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    [SerializeField] private Transform Target;
+    [SerializeField] private Vector3 offset = new Vector3(0, 0, -10f);
+    [SerializeField] private float smoothing = 1.0f;
+
+    void LateUpdate()
+    {
+        Vector3 newpos = Vector3.Lerp(transform.position, Target.position + offset, smoothing * Time.deltaTime);
+        transform.position = newpos;
+    }
+}

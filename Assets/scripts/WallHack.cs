b24c64ed529440684157cff0e6a087f2cca4001c
secondlnl <142692021+secondlnl@users.noreply.github.com>
2025-09-03 22:20:28 +0200

    WALL

---
+++ Assets/scripts/WallHack.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class WallHack : MonoBehaviour
+{
+    private Animator anim;
+    private bool HasPlayed;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            HasPlayed = true;
+            anim.SetTrigger("move");
+        }
+    }
+}

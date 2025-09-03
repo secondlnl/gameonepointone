e99a56ba3bba8e0795f1af22088cdfbdfb8ec8ab
secondlnl <142692021+secondlnl@users.noreply.github.com>
2025-09-03 21:38:03 +0200

    Platform

---
+++ Assets/scripts/PlatformMove.cs
@@ -0,0 +1,30 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+    [SerializeField] private Transform TargetEnd, TargetStart;
+    [SerializeField] private float moveSpeed = 2.0f;
+
+    private Transform CurrentTarget;
+    void Start()
+    {
+        CurrentTarget = TargetStart;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position == TargetStart.position) CurrentTarget = TargetEnd;
+        if (transform.position == TargetEnd.position) CurrentTarget = TargetStart;
+        transform.position = Vector2.MoveTowards(transform.position, CurrentTarget.position, moveSpeed * Time.deltaTime);
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && other.transform.position.y > transform.position.y) other.transform.SetParent(transform);
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) other.transform.SetParent(null);
+    }
+}

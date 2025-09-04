8e4a6acb87816dfdc5ffd0f845cbf85133ae2045
secondlnl <142692021+secondlnl@users.noreply.github.com>
2025-09-04 09:47:45 +0200

    Trampo

--- Assets/scripts/Trampo.cs
+++ Assets/scripts/Trampo.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Trampo : MonoBehaviour
 {
-    [SerializeField] private float JumpFore = 200f;
+    [SerializeField] private float JumpForce = 200f;
     
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -11,7 +11,7 @@ public class Trampo : MonoBehaviour
         {
             Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
             rb.linearVelocity = new Vector2(rb.linearVelocityX, 0);
-            rb.AddForce(new Vector2(0, JumpFore));
+            rb.AddForce(new Vector2(0, JumpForce));
             GetComponent<Animator>().SetTrigger("jump");
         }
     }

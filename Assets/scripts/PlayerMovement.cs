1d210228d04cfd003c775e36347b38f4edcf82ae
secondlnl <142692021+secondlnl@users.noreply.github.com>
2025-09-07 00:39:19 +0200

    Pink: added hits, removed DouJmp

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -11,6 +11,8 @@ public class movement : MonoBehaviour
     [SerializeField] private LayerMask Grounded;
     [SerializeField] private Transform SpawnPosition;
     [SerializeField] private AudioClip[] JumpSounds;
+    [SerializeField] private AudioClip[] HitSounds;
+
     [SerializeField] private AudioClip PickupSound;
     [SerializeField] private GameObject CherryPart, JumpPart;
 
@@ -21,8 +23,11 @@ public class movement : MonoBehaviour
 
     private float horizontalValue = 0f;
     private float rayDistance = 0.25f;
-    private bool OnGround;
     private bool canMove;
+    // private bool DoubleJumping;
+    // private bool Jumped;
+    // private bool DoubleJumped;
+
     [SerializeField] private int StartingHealth = 5;
     private int CurrentHealth = 0;
     public int CherryCount = 0;
@@ -44,16 +49,26 @@ public class movement : MonoBehaviour
 
     void Update()
     {
-//        print(CurrentHealth);
+        print("cm: " + canMove);
+        anim.SetBool("Grounded", CheckGround());
+        //        print(CurrentHealth);
         horizontalValue = Input.GetAxis("Horizontal");
         if (horizontalValue < 0) FlipSprite(true);
         if (horizontalValue > 0) FlipSprite(false);
 
-        if (Input.GetButtonDown("Jump") && CheckGround() == true) Jump();
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (CheckGround() == true /*&& Jumped == false*/)
+            {
+                Jump();
+                // Jumped = true;
+            }
+            // else if (Jumped == true && CheckGround() == false && DoubleJumped == false) DoubleJump();
+        }
 
         anim.SetFloat("MoveSpeed", Mathf.Abs(rb.linearVelocityX));
         anim.SetFloat("VerticalSpeed", rb.linearVelocityY);
-        anim.SetBool("Grounded", CheckGround());
+        // anim.SetBool("doublejumping", DoubleJumping);
 
     }
     void FixedUpdate()
@@ -93,6 +108,22 @@ public class movement : MonoBehaviour
         audi.PlayOneShot(JumpSounds[RandomValue], 0.5f);
         Instantiate(JumpPart, transform.position, JumpPart.transform.localRotation);
     }
+    // private void DoubleJump()
+    // {
+    //     DoubleJumping = true;
+    //     DoubleJumped = true;
+    //     rb.AddForce(new Vector2(0, JumpForce));
+    //     int RandomValue = Random.Range(0, JumpSounds.Length /*+ 1*/);
+    //     print(RandomValue);
+    //     audi.PlayOneShot(JumpSounds[RandomValue], 0.5f);
+    //     Invoke("CanDoublejumpAgain", 1.4f);
+    // }
+    // private void CanDoublejumpAgain()
+    // {
+    //     DoubleJumping = false;
+    //     DoubleJumped = false;
+    //     Jumped = false;
+    // }
     public void TakeDMG(int DMGamount)
     {
         CurrentHealth -= DMGamount;
@@ -104,6 +135,9 @@ public class movement : MonoBehaviour
     public void TakeKnockback(float knockbackForce, float updraft)
     {
         anim.SetTrigger("hit");
+        int RandomHit = Random.Range(0, HitSounds.Length /*+ 1*/);
+        audi.pitch = Random.Range(0.8f, 1.2f);
+        audi.PlayOneShot(HitSounds[RandomHit], 0.5f);
         canMove = false;
         rb.AddForce(new Vector2(knockbackForce, updraft));
         Invoke("CanMoveAgain", 0.25f);
@@ -127,7 +161,7 @@ public class movement : MonoBehaviour
         TUT 9 Fix
         Heal can't be picked up if it would heal too much  
         */
-        int healthToRestore = healthPickUp.GetComponent<health>().HealPoints; 
+        int healthToRestore = healthPickUp.GetComponent<health>().HealPoints;
         if (CurrentHealth >= StartingHealth || (CurrentHealth + healthToRestore) > StartingHealth) return;
         else
         {
@@ -147,10 +181,13 @@ public class movement : MonoBehaviour
     private bool CheckGround()
     {
         RaycastHit2D leftHit = Physics2D.Raycast(LeftFoot.position, Vector2.down, rayDistance, Grounded);
-        print(leftHit.collider.tag);
-
         RaycastHit2D RightHit = Physics2D.Raycast(RightFoot.position, Vector2.down, rayDistance, Grounded);
-        if (leftHit.collider != null && leftHit.collider.CompareTag("Ground") || RightHit.collider != null && RightHit.collider.CompareTag("Ground")) return true; else return false;
+
+        if (leftHit.collider != null && leftHit.collider.CompareTag("Ground") || RightHit.collider != null && RightHit.collider.CompareTag("Ground"))
+        {
+            return true;
+        }
+        else return false;
 
     }
 

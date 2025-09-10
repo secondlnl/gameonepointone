07e6e4ce53ed125334496ef1b414579162900643
secondlnl <142692021+secondlnl@users.noreply.github.com>
2025-09-10 22:44:16 +0200

    Add castletrigger script for scene transition
    Introduces castletrigger.cs, a Unity MonoBehaviour that detects player collision, disables movement, plays an animation, and loads a new scene after a delay.

---
+++ Assets/castletrigger.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class castletrigger : MonoBehaviour
+{
+    [SerializeField] private Camera Cam;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            other.gameObject.GetComponent<movement>().enabled = false;
+            other.gameObject.GetComponent<Animator>().Play("PinkRun", -1);
+            other.gameObject.GetComponent<Animator>().SetFloat("MoveSpeed", 0.2f);
+            Invoke("LoadLevel", 3.21f);
+            
+        }
+    }
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+}

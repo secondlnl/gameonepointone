b8e999b08d5b80a174e03bdf6f2518eaa26e5c9b
secondlnl <142692021+secondlnl@users.noreply.github.com>
2025-09-10 22:44:54 +0200

    Added a pause menu

---
+++ Assets/minimenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class minimenu : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/pause.cs
@@ -0,0 +1,29 @@
+using TMPro.EditorUtilities;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class pause : MonoBehaviour
+{
+    public bool Paused = false;
+    [SerializeField] private GameObject Panel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            Panel.SetActive(Paused);
+        if (Paused == true)
+        {
+            Time.timeScale = 0.0f;
+        }
+        else Time.timeScale = 1f;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Paused = !Paused;
+        }
+    }
+}

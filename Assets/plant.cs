9fe497230c3c3765e61f156350c49db4a55bd144
secondlnl <142692021+secondlnl@users.noreply.github.com>
2025-09-10 11:42:02 +0200

    Plant code done AKA WHY NOT SEPARATE

--- Assets/plant.cs
+++ Assets/plant.cs
@@ -6,7 +6,14 @@ public class plant : MonoBehaviour
     [SerializeField] private GameObject bullet;
     [SerializeField] private LayerMask Player;
     [SerializeField] private float rayDistance = 2f;
+    [SerializeField] private float bounciness = 300f;
     [SerializeField] private Transform HitRay;
+    [SerializeField] private float KnockbackForce = 400f;
+    [SerializeField] private float Updraft = 180f;
+    [SerializeField] private int DMGGiven = 1;
+    [SerializeField] private AudioClip HitSound;
+    [SerializeField] private GameObject PlantPart;
+    private bool dead = false;
     private bool toggle = true;
     private Animator anim;
     [SerializeField] private Vector3 offset = new Vector3(5f, 0);
@@ -44,5 +51,36 @@ public class plant : MonoBehaviour
             return true;
         }
         else return false;
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<movement>().TakeDMG(DMGGiven);
+
+            if (other.transform.position.x > transform.position.x)
+            {
+                other.gameObject.GetComponent<movement>().TakeKnockback(KnockbackForce, Updraft);
+            }
+            else { other.gameObject.GetComponent<movement>().TakeKnockback(-KnockbackForce, Updraft); }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(other.GetComponent<Rigidbody2D>().linearVelocityX, 0);
+            other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, bounciness));
+            other.GetComponent<AudioSource>().PlayOneShot(HitSound, 0.5f);
+            GetComponent<Animator>().SetTrigger("hit");
+            Instantiate(PlantPart, transform.position, PlantPart.transform.localRotation);
+
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<CircleCollider2D>().enabled = false;
+            GetComponent<Rigidbody2D>().gravityScale = 0;
+            GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+            dead = true;
+            Destroy(gameObject, 0.6f);
+        }
     }
 }

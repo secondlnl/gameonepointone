c0be474cf862df36c7f3dda60dba64c599348d2c
secondlnl <142692021+secondlnl@users.noreply.github.com>
2025-09-15 20:05:32 +0200

    blah

--- Assets/castletrigger.cs
+++ Assets/castletrigger.cs
@@ -17,7 +17,7 @@ public class castletrigger : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")) 
         {
 
             other.gameObject.GetComponent<movement>().enabled = false;

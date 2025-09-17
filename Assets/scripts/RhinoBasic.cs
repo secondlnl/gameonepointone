6625e3ef50ebfbc431e233cc8660640b3fcfe452
AlexanderFuru <doikarn94@gmail.com>
2025-09-17 19:13:20 +0200

    Update RhinoBasic.cs
    Added code for "Player" detection with RayCast

--- Assets/scripts/RhinoBasic.cs
+++ Assets/scripts/RhinoBasic.cs
@@ -7,8 +7,9 @@ public class RhinoBasic : MonoBehaviour
     [SerializeField] private float KnockbackForce = 400f;
     [SerializeField] private float Updraft = 180f;
     [SerializeField] private int DMGGiven = 1;
-    [SerializeField] private AudioClip HitSound;
+    [SerializeField] private LayerMask whatIsPlayer;
     [SerializeField] private Transform eyes;
+    [SerializeField] private AudioClip HitSound;
     private SpriteRenderer sr;
     private bool dead = false;
     private bool canSeePlayer;
@@ -26,6 +27,20 @@ public class RhinoBasic : MonoBehaviour
 
     void Update()
     {
-        
+        CanSeePlayer();
+    }
+
+    private bool CanSeePlayer()
+    {
+        RaycastHit2D playerSeen = Physics2D.Raycast(eyes.position, Vector2.left, rayDistance, whatIsPlayer);
+
+        if(playerSeen.collider != null && playerSeen.collider.CompareTag("Player"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }

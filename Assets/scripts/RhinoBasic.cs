960ae08f87a4861f91dd81efde363529ff5ffa0e
AlexanderFuru <doikarn94@gmail.com>
2025-09-17 19:07:21 +0200

    Added code from enemymove.
    Added various lines of code from the enemymovement script that involves movement, knockback and damage

--- Assets/scripts/RhinoBasic.cs
+++ Assets/scripts/RhinoBasic.cs
@@ -2,13 +2,28 @@ using UnityEngine;
 
 public class RhinoBasic : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float MoveSpeed = 2.0f;
+    [SerializeField] private float bounciness = 300f;
+    [SerializeField] private float KnockbackForce = 400f;
+    [SerializeField] private float Updraft = 180f;
+    [SerializeField] private int DMGGiven = 1;
+    [SerializeField] private AudioClip HitSound;
+    [SerializeField] private Transform eyes;
+    private SpriteRenderer sr;
+    private bool dead = false;
+    private bool canSeePlayer;
+    private float rayDistance = 25f;
+
+
+
+
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+
+
     void Update()
     {
         

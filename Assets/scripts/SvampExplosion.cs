ac0545d26f7ee0d587c033cdf126a548fe7a72af
AlexanderFuru <doikarn94@gmail.com>
2025-09-22 10:24:24 +0200

    Script explosion
    Added a script for the explosion to be able to collide with a "Player" collider and deal damage

---
+++ Assets/scripts/SvampExplosion.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SvampExplosion : MonoBehaviour
+{
+    [SerializeField] private float KnockbackForce = 400f;
+    [SerializeField] private float Updraft = 180f;
+    [SerializeField] private int DMGGiven = 1;
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerDamage>().TakeDMG(DMGGiven);
+
+            if (other.transform.position.x > transform.position.x)
+            {
+                other.gameObject.GetComponent<PlayerDamage>().TakeKnockback(KnockbackForce, Updraft);
+            }
+            else { other.gameObject.GetComponent<PlayerDamage>().TakeKnockback(-KnockbackForce, Updraft); }
+        }
+    }
+
+}
